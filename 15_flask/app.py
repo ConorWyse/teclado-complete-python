a65710c06f0b4c99fc9c1438a5dfc010ebe3ca5b
Conor Wyse <github@elmudo.freedom.nl>
2025-01-28 20:45:15 +0100

    :tada: Start a Flash-based project

---
+++ 15_flask/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+posts = {
+    0: {
+        'title': 'Hellow, world',
+        'content': 'This is my first blog post!'
+    }
+}
+
+
+@app.route('/')
+def home():
+    return 'Hello, world!'
+
+
+@app.route('/post/<int:post_id>')
+def get_post(post_id):
+    post = posts.get(post_id)
+    if not post:
+        return render_template('404.html', message=f'A post with id {post_id} was not found.')
+    #return f"Post {post['title']}, content:\n\n{post['content']}"
+    return render_template('post.html', post=post)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
