d7caf5dcdba227c95376591bd44a2487911eafca
Conor Wyse <github@elmudo.freedom.nl>
2025-02-01 19:34:41 +0100

    :sparkles: Add form to create new post
    Simple HTML form that allows the user to create a new post.

--- 15_flask/app.py
+++ 15_flask/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 
 app = Flask(__name__)
 posts = {
     0: {
+        'id': 0,
         'title': 'Hellow, world',
         'content': 'This is my first blog post!'
     }
@@ -22,6 +23,21 @@ def get_post(post_id):
         return render_template('404.jinja2', message=f'A post with id {post_id} was not found.')
     #return f"Post {post['title']}, content:\n\n{post['content']}"
     return render_template('post.jinja2', post=post)
+
+
+@app.route('/post/form')
+def form():
+    return render_template('create.jinja2')
+
+
+@app.route('/post/create')
+def create():
+    title = request.args.get('title')
+    content = request.args.get('content')
+    post_id = len(posts)
+    posts[post_id] = {'id': post_id, 'title': title, 'content': content}
+
+    return redirect(url_for('get_post', post_id=post_id))
 
 
 if __name__ == '__main__':
