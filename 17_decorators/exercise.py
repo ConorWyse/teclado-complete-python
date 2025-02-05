5e77097361f31bbf98d874c83adaec4c793657d0
Conor Wyse <github@elmudo.freedom.nl>
2025-02-05 19:48:25 +0100

    :technologist: Make decorator more dynamic and useful

--- 17_decorators/exercise.py
+++ 17_decorators/exercise.py
@@ -4,22 +4,24 @@ import functools
 user = {
     'id': 1,
     'name': 'jose',
-    'role': 'badmin'
+    'role': 'admin'
 }
 
 
 # Define a check_permission() decorator:
-def check_permission(func):
-    @functools.wraps(func)
-    def secure_func():
-        if user.get('role') == 'admin':
-            return func()
-        else:
-            return PermissionError
-    return secure_func
+def check_permission(required_role):
+    def check_permission_decorator(func):
+        @functools.wraps(func)
+        def secure_func():
+            if user.get('role') == required_role:
+                return func()
+            else:
+                return PermissionError
+        return secure_func
+    return check_permission_decorator
 
 
-@check_permission
+@check_permission('admin')
 def delete_database():
     """
     Will __doc__ show this or something from 'secure_func'?
@@ -29,5 +31,5 @@ def delete_database():
 
 delete_database()
 
-print(delete_database.__name__)
-print(delete_database.__doc__)
+# print(delete_database.__name__)
+# print(delete_database.__doc__)
